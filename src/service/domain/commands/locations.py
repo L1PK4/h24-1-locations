fb6f3c6f1e9e6abea74c5ec4e6b8571be77ed1d4
danya <ostrishko.danila@gmail.com>
2024-04-06 17:17:04 +0300

    feat: aliases

--- src/service/domain/commands/locations.py
+++ src/service/domain/commands/locations.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 from service.domain.models.locations import LocationType
 
 
 class LocationCommand(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
     lat: float
     lon: float
     type: LocationType
--- src/service/domain/views/locations.py
+++ src/service/domain/views/locations.py
@@ -1,10 +1,12 @@
 from typing import Self
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 from service.domain.models.locations import LocationType, Locations
 
 
 class LocationView(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
     id: int
     lat: float
     lon: float
