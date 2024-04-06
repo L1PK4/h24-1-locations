f79cf718b7f1bbc5bd755f6965865866416d7573
danya <ostrishko.danila@gmail.com>
2024-04-06 12:46:08 +0300

    fix: cors

--- src/service/__main__.py
+++ src/service/__main__.py
@@ -2,8 +2,16 @@ import asyncio
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from service.app.app import create_app
 
 if __name__ == "__main__":
     app = create_app()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     asyncio.run(serve(app, Config()))
